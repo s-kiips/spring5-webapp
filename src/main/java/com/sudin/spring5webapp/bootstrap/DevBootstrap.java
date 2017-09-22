6271bf9426fc4245d5840f1c9d1703a5c3b4fa11
s-kiips <ranjitkarsudeen14@gmail.com>
2017-09-22 21:43:11 +0545

    added Publisher entity

--- src/main/java/com/sudin/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/com/sudin/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.sudin.spring5webapp.bootstrap;
 
 import com.sudin.spring5webapp.model.Author;
 import com.sudin.spring5webapp.model.Book;
+import com.sudin.spring5webapp.model.Publisher;
 import com.sudin.spring5webapp.repositories.AuthorRepository;
 import com.sudin.spring5webapp.repositories.BookRepository;
+import com.sudin.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -15,10 +17,14 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository,PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+
     }
 
     @Override
@@ -28,9 +34,15 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
 
     private void initData() {
+
+        Publisher publisher=new Publisher();
+        publisher.setName("foo");
+        publisherRepository.save(publisher);
+
         //Sudin
         Author sudin = new Author("Sudin", "Ranjitkar");
-        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        Publisher publisherSudin=new Publisher("ranjitBooks","Sitapaila");
+        Book ddd = new Book("Domain Driven Design", "1234",publisher);
         sudin.getBooks().add(ddd);
         ddd.getAuthors().add(sudin);
         authorRepository.save(sudin);
@@ -39,7 +51,8 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //Sudeen
         Author sudeen = new Author("Sudeen", "Ranjitkar");
-        Book javaEE = new Book("J2EE Development", "24356", "sam");
+        Publisher publisherSudeen=new Publisher("ranjitkarSudeen","kathmandu");
+        Book javaEE = new Book("J2EE Development", "24356",publisher);
         sudeen.getBooks().add(javaEE);
         javaEE.getAuthors().add(sudeen);
         authorRepository.save(sudeen);
